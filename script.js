bb8ded1519181de1e7b2ddf5793c14a4dd8963cc
bogdan-lun-02 <bogdanlun36@gmail.com>
2024-07-23 15:04:06 +0300

    Started refactoring all code again

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-
-// function operate(firstNumber, operator, secondNumber) {
-// 	if (operator === '+') return add(firstNumber, secondNumber)
-// 	else if (operator === '-') return subtract(firstNumber, secondNumber)
-// 	else if (operator === '*') return multiply(firstNumber, secondNumber)
-// 	else if (operator === '/') return divide(firstNumber, secondNumber)
-// 	else return 'WRONG OPERATOR!'
-// }
-
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const equalButton = document.querySelector('.equal');
@@ -15,6 +6,7 @@ const display = document.querySelector('.display');
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
+let totalCount = '';
 
 // event listeners
 
@@ -26,35 +18,53 @@ operatorButtons.forEach(button => {
 	button.addEventListener('click', addOperator)
 })
 
-equalButton.addEventListener('click', () => {
-  if (operator === '+') display.textContent = add(firstNumber, secondNumber)
-	else if (operator === '-')
-		display.textContent = subtract(firstNumber, secondNumber)
-	else if (operator === '*')
-		display.textContent = multiply(firstNumber, secondNumber)
-	else if (operator === '/')
-		display.textContent = divide(firstNumber, secondNumber)
-});
+equalButton.addEventListener('click', operate);
 
 // function declarations
 
 function addNumbers(e) {
-  if (operator === '') {
-		firstNumber += e.target.value;
-    display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
-	} 
-  
-  else secondNumber += e.target.value;
-  display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+  if (totalCount !== '') {
+    secondNumber += e.target.value;
+    display.textContent = secondNumber;
+  }
+
+  else { if (operator === '') {
+    firstNumber += e.target.value;
+    display.textContent = firstNumber;
+  }
+  else if (operator !== '') {
+		secondNumber += e.target.value
+		display.textContent = secondNumber
+	}
+}
 }
 
 function addOperator(e) {
-	operator += e.target.value;
-  display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+  if (totalCount !== '') {
+    operator += e.target.value;
+    operate();
+  }
+	else {if (operator === '') {
+			operator += e.target.value;
+			e.target.style.backgroundColor = '#ad0a30';
+		} else {
+			operate()
+		}
+  }
 }
 
-const operateResult = '';
+function operate() {
+  if (operator === '+') {
+		totalCount = add(firstNumber, secondNumber)
+	} else if (operator === '-') {
+		totalCount = subtract(firstNumber, secondNumber)
+	}
 
+  display.textContent = totalCount;
+  firstNumber = totalCount;
+  operator = '';
+  secondNumber = '';
+}
 
 
 function add(x, y) {
